fc75d8bcdd83b97303e33dfeab967361835aa705
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-29 13:46:54 -0300

    finish exercise04

--- AdaTech_TP1.Ex.Aula03/Exercises/Exercise04.cs
+++ AdaTech_TP1.Ex.Aula03/Exercises/Exercise04.cs
@@ -1,4 +1,6 @@
-﻿namespace AdaTech_TP1.Ex.Aula03.Exercises
+﻿using System.Text.RegularExpressions;
+
+namespace AdaTech_TP1.Ex.Aula03.Exercises
 {
     internal class Exercise04 : Exercise
     {
@@ -6,12 +8,51 @@
         {
             try
             {
+                Console.WriteLine("Digite um texto:");
+                string text = Console.ReadLine();
 
+                text = NormalizeText(text);
+                string[] words = ExtractWords(text);
+                Dictionary<string, int> wordCount = CountWords(words);
+
+                DisplayWordFrequency(wordCount);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+        }
+
+        private string NormalizeText(string text)
+        {
+            return Regex.Replace(text.ToLower(), @"[^a-z\s]", " ");
+        }
+
+        private string[] ExtractWords(string text)
+        {
+            return Regex.Split(text.Trim(), @"\s+|\t+");
+        }
+
+        private Dictionary<string, int> CountWords(string[] words)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                    wordCount[word]++;
+                else
+                    wordCount[word] = 1;
+            }
+
+            return wordCount;
+        }
+
+        private void DisplayWordFrequency(Dictionary<string, int> wordCount)
+        {
+            Console.WriteLine("\nFrequência das Palavras:");
+            foreach (var item in wordCount)
+                Console.WriteLine($"{item.Key}: {item.Value} vez{(item.Value > 1 ? "es" : "")}");
         }
     }
 }
