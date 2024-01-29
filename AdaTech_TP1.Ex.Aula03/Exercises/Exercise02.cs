79d101eaa22d46376c6ab80e4af6886922117432
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-29 11:56:05 -0300

    finish exercise03

--- AdaTech_TP1.Ex.Aula03/Exercises/Exercise02.cs
+++ AdaTech_TP1.Ex.Aula03/Exercises/Exercise02.cs
@@ -6,12 +6,47 @@
         {
             try
             {
+                Console.Write("Digite a expressão matemática: ");
+                string expressao = Console.ReadLine();
 
+                Console.WriteLine(VerificarBalanceamento(expressao) ? "Expressão balanceada" : "Expressão não balanceada");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+        }
+
+        static bool VerificarBalanceamento(string expressao)
+        {
+            Stack<char> pilha = new Stack<char>();
+
+            foreach (char caractere in expressao)
+            {
+                if (caractere == '(' || caractere == '[' || caractere == '{')
+                {
+                    pilha.Push(caractere);
+                }
+                else if (caractere == ')' || caractere == ']' || caractere == '}')
+                {
+                    if (pilha.Count == 0)
+                        return false;
+
+                    char topoPilha = pilha.Pop();
+
+                    if (!SaoPares(topoPilha, caractere))
+                        return false;
+                }
+            }
+
+            return pilha.Count == 0;
+        }
+
+        static bool SaoPares(char abertura, char fechamento)
+        {
+            return (abertura == '(' && fechamento == ')') ||
+                   (abertura == '[' && fechamento == ']') ||
+                   (abertura == '{' && fechamento == '}');
         }
     }
 }
